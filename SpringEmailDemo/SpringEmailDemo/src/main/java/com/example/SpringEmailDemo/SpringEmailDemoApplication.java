b8e369a582bdb937e062354644011cc36162b9ee
madhusudhan1909 <124492728+madhusudhan1909@users.noreply.github.com>
2023-03-20 11:02:58 +0530

    Add files via upload

---
+++ SpringEmailDemo/SpringEmailDemo/src/main/java/com/example/SpringEmailDemo/SpringEmailDemoApplication.java
@@ -0,0 +1,25 @@
+package com.example.SpringEmailDemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+@SpringBootApplication
+public class SpringEmailDemoApplication {
+	@Autowired
+	private EmailSenderService senderService;
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringEmailDemoApplication.class, args);
+	}
+@EventListener(ApplicationReadyEvent.class)
+public void sendMail() {
+	senderService.sendEmail("email",
+			"This is subject",
+			"very cute boyyy" +
+					"	");
+}
+}
